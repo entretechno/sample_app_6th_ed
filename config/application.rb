3fc9564c4d5736406f1fe34cd1bf03e21890226b
Jeremiah <jeremiah@entretechno.com>
2022-04-04 16:17:33 -0500

    Configure app for AWS Cloud9

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,9 @@ module SampleApp
 
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.action_dispatch.default_headers.delete('X-Frame-Options')
+
+    config.action_controller.allow_forgery_protection = false
   end
 end
